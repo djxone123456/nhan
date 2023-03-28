245d428a0168e5cfb77dba81701ebab587b94a4d
Nguyễn Đình Nhân <46621320+djxone123456@users.noreply.github.com>
2023-03-29 00:57:00 +0700

    Add files via upload
    Done menu + new game + settings + help

---
+++ Caro/About.h
@@ -0,0 +1,14 @@
+#pragma once
+#pragma comment(lib, "winmm.lib")
+
+#ifndef ABOUT_H
+#define ABOUT_H
+#include "Control.h"
+#include "View.h"
+#include "Data.h"
+#include "D2_data.h"
+
+int About();
+void HandleKeyForAbout(int X, int Y, KEY_EVENT_RECORD key);
+
+#endif // ABOUT_H
